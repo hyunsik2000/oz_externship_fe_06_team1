ed0f690e4d1ce3b3d0e9ac769b0844a52709a6b4
hyunsik2000 <ryuhsik2000@naver.com>
2026-01-22 14:34:01 +0900

    feat: exams 목 데이터 추가
    테스트용 예시 데이터 입니다.
    
    #16

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,11 @@
-// handlers.ts
 import { http, HttpResponse } from 'msw'
+import { MOCK_EXAM_LIST_RESPONSE } from './table-data/ExamList'
 
 export const handlers = [
   http.get('/api/hello', () => {
     return HttpResponse.json({ message: 'Hello, world!', code: 200 })
+  }),
+  http.get('/api/exams', () => {
+    return HttpResponse.json(MOCK_EXAM_LIST_RESPONSE)
   }),
 ]
