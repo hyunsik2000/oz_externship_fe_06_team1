4bfe72bce87aad8cef21f993e5707b0a5511c5c9
Jay <jiyeyoun@outlook.com>
2026-01-23 15:39:39 +0900

    feat: 앱 라우팅 설정 (#35)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router'
 import AdminLayout from '@/components/layout/AdminLayout'
+import { NotFoundPage } from '@/pages/NotFoundPage'
 
 export default function App() {
-  return <AdminLayout />
+  return (
+    <Routes>
+      <Route element={<AdminLayout />}>
+        <Route path="/" />
+      </Route>
+      <Route path="/404" element={<NotFoundPage />} />
+    </Routes>
+  )
 }
