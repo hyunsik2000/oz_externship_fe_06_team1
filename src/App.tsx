a10fe50164bc906bce5e2e522bd046ab92d258e5
권희정 <heejeongkwon@gwonhuijeong-ui-MacBookPro.local>
2026-01-28 13:30:07 +0900

    feat: 응시내역관리 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import AdminLayout from '@/components/layout/AdminLayout'
 import { NotFoundPage } from '@/pages/NotFoundPage'
 import { ExamDashboardPage } from './pages/ExamDashboardPage'
+import ExamHistoryPage from '@/pages/ExamHistoryPage'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route element={<AdminLayout />}>
         <Route index element={<Navigate to="/members" replace />} />
         <Route path="/dashboard" element={<ExamDashboardPage />} />
+        <Route path="/exam/history" element={<ExamHistoryPage />} />
         {/* <Route path="deployments" element={<DeploymentPage />} /> */}
       </Route>
 
