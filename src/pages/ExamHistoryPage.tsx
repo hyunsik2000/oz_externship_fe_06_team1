ac4900b24703e0b72d69fbf34d8cd37c18d6009b
권희정 <heejeongkwon@gwonhuijeong-ui-MacBookPro.local>
2026-01-30 14:11:16 +0900

    feat: 응시내역 상단 FilterButton 배치

--- src/pages/ExamHistoryPage.tsx
+++ src/pages/ExamHistoryPage.tsx
@@ -1,9 +1,13 @@
+import { FilterButton } from '@/components/common/FilterButton'
 import { ExamHistoryLayout } from '@/components/layout/ExamHistoryLayout'
 import HistoryList from '@/components/table/HistoryList'
 
 export function ExamHistoryPage() {
   return (
-    <ExamHistoryLayout title="쪽지시험 응시 내역 조회">
+    <ExamHistoryLayout
+      title="쪽지시험 응시 내역 조회"
+      headerRight={<FilterButton />}
+    >
       <HistoryList />
     </ExamHistoryLayout>
   )
