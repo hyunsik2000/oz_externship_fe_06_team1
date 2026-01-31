014a1c1816bf4371b78ad7ac91b4e54bb8061a1f
hyunsik2000 <ryuhsik2000@naver.com>
2026-01-31 20:18:30 +0900

    feat: 쪽지시험 조회 페이지, 레이아웃 생성 (#82)
    쪽지시험 생성을 버튼과 모달을 확인 할 수 있는 쪽지시험 페이지 레이아웃 생성
    
    #82

---
+++ src/pages/exam-page/exam-list/ExamListPage.tsx
@@ -0,0 +1,59 @@
+import { Button, FilterButton, Input } from '@/components/common'
+import { ExamListLayout } from '@/components/layout'
+import ExamList from '@/components/table/examList'
+import { useState } from 'react'
+import ExamModal from '@/components/detail-exam/ExamModal/ExamModal'
+import { MOCK_EXAM_LIST_RESPONSE } from '@/mocks/data/table-data/ExamList'
+
+export function ExamListPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  return (
+    <>
+      <ExamListLayout
+        title="쪽지시험 조회"
+        toolbar={
+          <div className="flex w-full items-center justify-between gap-2">
+            <div className="flex gap-2">
+              <Input
+                placeholder="검색어를 입력하세요"
+                className="h-9 w-[250px]"
+              />
+              <Button
+                variant="secondary"
+                onClick={() => {}} // TODO: 검색 기능 추가
+                className="h-9 w-[70px] rounded-sm text-sm font-normal"
+              >
+                조회
+              </Button>
+            </div>
+            <FilterButton />
+          </div>
+        }
+        footer={
+          <div className="flex w-full justify-end gap-2">
+            <Button
+              variant="primary"
+              onClick={() => setIsModalOpen(true)}
+              className="h-[36px] w-[55px] rounded-sm text-sm font-normal"
+            >
+              생성
+            </Button>
+          </div>
+        }
+      >
+        <ExamList data={MOCK_EXAM_LIST_RESPONSE.exams} />
+      </ExamListLayout>
+
+      <ExamModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        mode="create"
+        initialData={{
+          title: '',
+          subject_name: '',
+          logo_url: '',
+        }}
+      />
+    </>
+  )
+}
