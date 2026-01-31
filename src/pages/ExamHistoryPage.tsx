4b6417636a79d2e5930f018b5719ce005d4f3162
권희정 <heejeongkwon@gwonhuijeong-ui-MacBookPro.local>
2026-01-31 15:13:00 +0900

    chore: 응시내역 페이지 Mock 데이터 제거

--- src/pages/ExamHistoryPage.tsx
+++ src/pages/ExamHistoryPage.tsx
@@ -1,14 +1,52 @@
+import { useState } from 'react'
+import CourseSubjectFilterModal, {
+  type FilterValue,
+  type Option,
+} from '@/components/common/CourseSubjectFilterModal'
 import { FilterButton } from '@/components/common/FilterButton'
 import { ExamHistoryLayout } from '@/components/layout/ExamHistoryLayout'
 import HistoryList from '@/components/table/HistoryList'
 
 export function ExamHistoryPage() {
+  const [isFilterOpen, setIsFilterOpen] = useState(false)
+  const [filter, setFilter] = useState<FilterValue>({
+    course: '',
+    cohort: '',
+    subject: '',
+  })
+
+  const courseOptions: Option[] = []
+  const cohortOptions: Option[] = []
+  const subjectOptions: Option[] = []
+
+  const handleOpenFilter = () => setIsFilterOpen(true)
+  const handleCloseFilter = () => setIsFilterOpen(false)
+
+  const handleChangeFilter = (next: FilterValue) => setFilter(next)
+
+  const handleSubmitFilter = () => {
+    setIsFilterOpen(false)
+  }
+
   return (
-    <ExamHistoryLayout
-      title="쪽지시험 응시 내역 조회"
-      headerRight={<FilterButton />}
-    >
-      <HistoryList />
-    </ExamHistoryLayout>
+    <>
+      <ExamHistoryLayout
+        title="쪽지시험 응시 내역 조회"
+        headerRight={<FilterButton onClick={handleOpenFilter} />}
+      >
+        <HistoryList />
+      </ExamHistoryLayout>
+
+      <CourseSubjectFilterModal
+        open={isFilterOpen}
+        onClose={handleCloseFilter}
+        courseOptions={courseOptions}
+        cohortOptions={cohortOptions}
+        subjectOptions={subjectOptions}
+        value={filter}
+        onChange={handleChangeFilter}
+        onSubmit={handleSubmitFilter}
+      />
+    </>
   )
 }
