9e546717e3e7a9e729a02a041f1acda3774769f8
hyunsik2000 <ryuhsik2000@naver.com>
2026-02-03 14:13:45 +0900

    feat: 쪽지시험 조회에 FilterModal 적용 (#104)

--- src/pages/exam-page/exam-list/ExamListPage.tsx
+++ src/pages/exam-page/exam-list/ExamListPage.tsx
@@ -1,12 +1,38 @@
-import { Button, FilterButton, Input } from '@/components/common'
+import { Button, FilterButton, FilterModal, Input } from '@/components/common'
 import { ExamListLayout } from '@/components/layout'
 import ExamList from '@/components/table/ExamList'
 import { useState } from 'react'
 import ExamModal from '@/components/detail-exam/exam-modal/ExamModal'
 import { MOCK_EXAM_LIST_RESPONSE } from '@/mocks/data/table-data/ExamList'
+import { useFilter, type FilterOptionConfig } from '@/hooks/useFilter'
+import { COURSE_OPTIONS } from '@/constants/filtered-option'
 
 export function ExamListPage() {
   const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const filterConfigs: FilterOptionConfig[] = [
+    {
+      key: 'course',
+      label: '',
+      placeholder: '과정을 선택해주세요',
+      options: COURSE_OPTIONS,
+    },
+  ]
+
+  const {
+    isOpen: isFilterOpen,
+    openFilter: handleOpenFilter,
+    closeFilter: handleCloseFilter,
+    values: filter, // 필터 값
+    rows, // 필터 행
+    summary, // 필터 요약
+    canSubmit, // 필터 제출 가능 여부
+  } = useFilter(filterConfigs)
+
+  const handleSubmitFilter = () => {
+    handleCloseFilter()
+  }
+
   return (
     <>
       <ExamListLayout
@@ -18,15 +44,11 @@ export function ExamListPage() {
                 placeholder="검색어를 입력하세요"
                 className="h-9 w-[250px]"
               />
-              <Button
-                variant="secondary"
-                onClick={() => {}} // TODO: 검색 기능 추가
-                className="h-9 w-[70px] rounded-sm text-sm font-normal"
-              >
+              <Button variant="search" size="search" className="flex-shrink-0">
                 조회
               </Button>
             </div>
-            <FilterButton />
+            <FilterButton onClick={handleOpenFilter} />
           </div>
         }
         footer={
@@ -43,6 +65,17 @@ export function ExamListPage() {
       >
         <ExamList data={MOCK_EXAM_LIST_RESPONSE.exams} />
       </ExamListLayout>
+
+      <FilterModal
+        open={isFilterOpen}
+        onClose={handleCloseFilter}
+        title="과정별 필터링"
+        subtitle="필터를 적용할 카테고리를 선택해주세요."
+        rows={rows}
+        summary={summary}
+        onSubmit={handleSubmitFilter}
+        canSubmit={canSubmit}
+      />
 
       <ExamModal
         isOpen={isModalOpen}
