773503fa61c0c3a51ab0048fa42d45be083e693e
Jay <jiyeyoun@outlook.com>
2026-02-06 15:57:57 +0900

    feat: App.tsx 파일과 사이드바에 라우팅 연결 (#42)

--- src/components/layout/AdminSidebar.tsx
+++ src/components/layout/AdminSidebar.tsx
@@ -71,6 +71,10 @@ export function AdminSidebar() {
       navigate('/members/student-registration')
     }
 
+    if (sectionKey === 'member' && text === '회원 탈퇴 관리') {
+      navigate('/members/withdrawal')
+    }
+
     if (sectionKey === 'member' && text === '대시보드') {
       navigate('/members/dashboard')
     }
@@ -127,6 +131,9 @@ export function AdminSidebar() {
                     (pathname === '/members/student-registration' &&
                       section.key === 'member' &&
                       text === '수강생 등록 신청') ||
+                    (pathname === '/members/withdrawal' &&
+                      section.key === 'member' &&
+                      text === '회원 탈퇴 관리') ||
                     (pathname === '/members/dashboard' &&
                       section.key === 'member' &&
                       text === '대시보드')
@@ -138,6 +145,7 @@ export function AdminSidebar() {
                     (section.key === 'member' &&
                       (text === '유저 관리' ||
                         text === '수강생 등록 신청' ||
+                        text === '회원 탈퇴 관리' ||
                         text === '대시보드'))
 
                   return (
